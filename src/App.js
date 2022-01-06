a1fcba278fb0fcf2b5a6d326da18e09a6a16b312
pivovarof <lgagrog@gmail.com>
2022-01-06 12:49:05 +0200

    HW1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 import  Profile  from "./components/Profile/index";
 
@@ -7,7 +8,7 @@ import  Profile  from "./components/Profile/index";
 function App() {
   return (
     <div className="App">
-      
+      <Header/>      
       <Profile />
     </div>
      
